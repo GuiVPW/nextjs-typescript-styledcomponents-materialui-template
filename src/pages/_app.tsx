5950facc13f7cc251f666150fa09ad885c0f89e6
Guilherme Vieira <guivpw68@gmail.com>
2022-01-11 00:47:25 -0300

    feat: add nextjs _app page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import { materialTheme } from '../styles/material-theme'
+
+export default function MyApp(props: AppProps) {
+	const { Component, pageProps } = props
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Next App</title>
+				<link href="/favicon.ico" rel="icon" />
+				<meta
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+					name="viewport"
+				/>
+			</Head>
+			<ThemeProvider theme={materialTheme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</React.Fragment>
+	)
+}
